659842a5487db17ab0214c9338f8b8d5c7752064
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-04-15 20:02:22 -0600

    update: __main__.py
    Include program class and implement a basic run method, and variables
    that I'm going to use to solve this problem.

--- __main__.py
+++ __main__.py
@@ -1,6 +1,51 @@
-def main():
-    pass
+import random as r
+import numpy as np
+from colorama import init, Fore, Style
+from rich.console import Console
+from rich.table import Table
+
+
+class Program():
+    NUMS_OF_DAYS = 5
+    NUMS_OF_CLASSROOMS = 5
+    NUMS_OF_TEACHERS = 5
+    NUMS_OF_BLOCKS = 7
+    NUM_OF_SUBJECTS = 5
+    POPULATION_SIZE = 50
+    NUM_OF_GENRATIONS = 100
+    MUTATION_RATE = 0.1
+
+    subjects = {
+        1: "Matemáticas",
+        2: "Historia",
+        3: "Quimica",
+        4: "Física",
+        5: "Biología"
+    }
+
+    classrooms = {
+        1: "A113",
+        2: "B005",
+        3: "C001",
+        4: "C002",
+        5: "D005"
+    }
+
+    teachers = {
+        1: "Jorge",
+        2: "Juan",
+        3: "Elias",
+        4: "Aaron",
+        5: "Haidee"
+    }
+
+    def run(self):
+        init(autoreset=True)
+
+        print(
+            F"{Fore.LIGHTBLUE_EX}¡Bienvenido al generador de horarios usando algoritmos genéticos!")
 
 
 if __name__ == "__main__":
-    main()
+    program = Program()
+    program.run()
